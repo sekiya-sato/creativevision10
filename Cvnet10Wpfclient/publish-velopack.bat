f18a7659fd038d358b036e7e8fec2dc9bb0ae7df
Sekiya Sato (home) <sekiya.sato@gmail.com>
2026-04-05 15:18:28 +0900

    ログ出力フォルダの整備

--- Cvnet10Wpfclient/publish-velopack.bat
+++ Cvnet10Wpfclient/publish-velopack.bat
@@ -20,7 +20,8 @@ if errorlevel 1 (
 
 if exist "%PUBLISH_DIR%" rmdir /s /q "%PUBLISH_DIR%"
 
-dotnet publish "%PROJECT_DIR%Cvnet10Wpfclient.csproj" -c Release -r win-x64 --self-contained true -o "%PUBLISH_DIR%" /p:Version=%APP_VERSION% /p:FileVersion=%APP_VERSION% /p:InformationalVersion=%APP_VERSION%
+rem /p:Version -> Json type convert error=AssemblyVersion not equal
+dotnet publish "%PROJECT_DIR%Cvnet10Wpfclient.csproj" -c Release -r win-x64 --self-contained true -o "%PUBLISH_DIR%" /p:FileVersion=%APP_VERSION% /p:InformationalVersion=%APP_VERSION%
 if errorlevel 1 exit /b 1
 
 pushd "%PROJECT_DIR%"
