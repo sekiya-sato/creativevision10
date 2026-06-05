7e4ff1e1843d5b9bcfbf4381257f9196caea3508
Sekiya Sato (home) <sekiya.sato@gmail.com>
2026-06-05 18:56:14 +0900

    AIコーディングでの不足ファイルを追加

---
+++ CvWpfclient/Views/01Master/PrintMasterShainCardView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CvWpfclient.Views._01Master;
+
+/// <summary>
+/// </summary>
+public partial class PrintMasterShainCardView : Helpers.BaseWindow {
+	public PrintMasterShainCardView() {
+		InitializeComponent();
+	}
+}
