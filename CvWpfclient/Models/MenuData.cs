9598838a30ef22afd8150f7843b627412ff57255
Sekiya Sato <sekiya.sato@gmail.com>
2026-07-03 15:17:08 +0900

    不要メニューを削除

--- CvWpfclient/Models/MenuData.cs
+++ CvWpfclient/Models/MenuData.cs
@@ -52,8 +52,8 @@ public partial class MenuData : ObservableObject {
 		new("管理者用システム処理", typeof(Views._00System.SysExecMiscView), addInfo:"管理者用の各種システム処理"),
         /* ================================ */
 			new("----------------", typeof(string) , addInfo:"---"),
-			new("サンプル画面 ---", typeof(SampleView), addInfo:"-- MaterialDesignサンプル"),
-			new("Test画面--", typeof(Views._06Uriage.ShopUriageInputView), addInfo:"-- 一時的なテストで使用"),
+			new("後で削除: サンプル画面 ---", typeof(SampleView), addInfo:"-- MaterialDesignサンプル"),
+			new("後で削除: Test画面--", typeof(Views._06Uriage.ShopUriageInputView), addInfo:"-- 一時的なテストで使用"),
 		])),
 		new("■ マスター", new([
 			new("システム管理マスタ", typeof(Views._01Master.MasterSysKanriMenteView),  addInfo:"会社情報、締日、税率などを設定"),
@@ -70,9 +70,9 @@ public partial class MenuData : ObservableObject {
 			new("商品バーコードブック", typeof(Views._01Master.MasterPrintBarcodeView), addInfo:"商品バーコードブック印刷"),
 			new("取込レイアウト作成", typeof(Views._01Master.ImportTemplateCreateView), addInfo:"テーブル列定義からUTF-8 CSV取込レイアウトを作成"),
 			new("外部CSVマスタ取込", typeof(Views._01Master.ExternalCsvImportView), addInfo:"取込レイアウトCSVを検証してInsertBulkParamで登録"),
-			new("上代一括変更", typeof(Views._01Master.MasterJouDaiBulkChangeView), addInfo:"準備中"),
-			new("原価変更登録", typeof(Views._01Master.GenkaChangeEntryView), addInfo:"準備中"),
-			new("評価替", typeof(Views._01Master.ProductRatingChangeView), addInfo:"準備中"),
+			new("AfterToDo: 上代一括変更", typeof(Views._01Master.MasterJouDaiBulkChangeView), addInfo:"準備中 他のがだいたい終わってから実装する"),
+			new("AfterToDo: 原価変更登録", typeof(Views._01Master.GenkaChangeEntryView), addInfo:"準備中 他のがだいたい終わってから実装する"),
+			new("AfterToDo: 評価替", typeof(Views._01Master.ProductRatingChangeView), addInfo:"準備中 他のがだいたい終わってから実装する"),
 		])),
         /* ================================ */
 		new("■ 予算", new([
@@ -80,7 +80,6 @@ public partial class MenuData : ObservableObject {
 			new("店ブランド予算マスタメンテ", typeof(Views._02Yosan.MasterYosanBrandMenteView), addInfo:"MasterYosanBrand の日別予算レコードを直接編集"),
 			new("販売員別予算マスタ(月)", typeof(Views._02Yosan.SalesStaffBudgetMasterView), addInfo:"販売員別の月毎の日予算を作成"),
 			new("販売員予算マスタメンテ", typeof(Views._02Yosan.MasterYosanHanbaiMenteView), addInfo:"MasterYosanHanbai の日別予算レコードを直接編集"),
-			new("営業担当別予算マスタ", typeof(Views._02Yosan.SalesRepBudgetMasterView), addInfo:"準備中"),
 			new("店舗予算表", typeof(Views._02Yosan.ShopBudgetReportView), addInfo:"準備中"),
 			new("店舗ブランド別予算実績対比", typeof(Views._02Yosan.ShopBrandBudgetVsActualView), addInfo:"準備中"),
 			new("日別店別予算表", typeof(Views._02Yosan.DailyShopBudgetReportView), addInfo:"準備中"),
--- CvWpfclient/Views/02Yosan/SalesRepBudgetMasterView.xaml.cs
+++
@@ -1,7 +0,0 @@
-namespace CvWpfclient.Views._02Yosan;
-
-public partial class SalesRepBudgetMasterView : Helpers.BaseWindow {
-	public SalesRepBudgetMasterView() {
-		InitializeComponent();
-	}
-}
