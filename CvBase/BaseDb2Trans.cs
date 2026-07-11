40985eb42fd05ed99700d251e411250ba9f0cce1
Sekiya Sato (home) <sekiya.sato@gmail.com>
2026-07-11 15:03:41 +0900

    レイアウト調整

--- CvBase/BaseDb2Trans.cs
+++ CvBase/BaseDb2Trans.cs
@@ -437,7 +437,7 @@ public sealed partial class TranKinMeisai : ObservableObject {
 	/// 区分ユニークキー
 	/// </summary>
 	[ObservableProperty]
-	[ForeignKey(nameof(MasterMeisho))]
+	[ForeignKey(nameof(MasterMeisho))] // Kubun='KIN' を参照
 	public partial long Id_Kin { get; set; }
 	/// <summary>
 	/// 入金・支払CD
