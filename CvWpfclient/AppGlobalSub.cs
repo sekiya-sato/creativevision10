d4ecea9b8e25762587ec769a43a59bd9e9f42837
Sekiya Sato (home) <sekiya.sato@gmail.com>
2026-07-26 09:46:42 +0900

    標準倉庫の取得を追加

--- CvWpfclient/AppGlobalSub.cs
+++ CvWpfclient/AppGlobalSub.cs
@@ -1,18 +1,7 @@
 using CodeShare;
 using CvAsset;
 using CvBase;
-using CvBase.Share;
-using CvWpfclient.Helpers;
-using CvWpfclient.Models;
-using Grpc.Core;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using ProtoBuf.Grpc;
-using System.Collections.Concurrent;
-using System.Globalization;
-using System.IO;
 
 
 namespace CvWpfclient;
@@ -31,7 +20,18 @@ public static partial class AppGlobal {
 	async public static Task<int> LogicGetTax(int no, string date_ymd) {
 		int tax = 10; // デフォルト値 消費税10%
 		if (sysman == null) {
-			var param = new QueryByIdParam(typeof(MasterSysman),1);
+			await LogicGetSysman();
+		}
+		var systax = sysman?.Jsub?.Where(x => x.Id == no).FirstOrDefault() ?? new MasterSysTax();
+		tax = systax.TaxRate;
+		if (Common.CompareYmd(date_ymd, systax.DateFrom) >= 0) {
+			tax = systax.TaxNewRate;
+		}
+		return tax;
+	}
+	async public static Task<MasterSysman> LogicGetSysman() {
+		if (sysman == null) {
+			var param = new QueryByIdParam(typeof(MasterSysman), 1);
 			var msg = new CvMsg {
 				Code = 0,
 				Flag = CvFlag.Msg101_Op_Query,
@@ -41,21 +41,26 @@ public static partial class AppGlobal {
 			var coreService = AppGlobal.GetGrpcService<ICoreService>();
 			var reply = await coreService.QueryMsgAsync(msg, AppGlobal.GetDefaultCallContext());
 			if (reply.Code < 0) {
-				return tax;
+				return new MasterSysman();
 			}
 			var des = JsonConvert.DeserializeObject<MasterSysman>(reply.DataMsg);
 			sysman = des ?? new MasterSysman();
-			if(sysman.Jsub == null || sysman.Jsub.Count==0) {
-				sysman.Jsub = new List<MasterSysTax>();
-				return tax;
+			if (sysman.Id_Soko > 0) {
+				param = new QueryByIdParam(typeof(MasterTokui), sysman.Id_Soko);
+				msg = new CvMsg {
+					Code = 0,
+					Flag = CvFlag.Msg101_Op_Query,
+					DataType = typeof(QueryByIdParam),
+					DataMsg = Common.SerializeObject(param)
+				};
+				var reply2 = await coreService.QueryMsgAsync(msg, AppGlobal.GetDefaultCallContext());
+				if (reply2.Code >= 0) {
+					var des2 = JsonConvert.DeserializeObject<MasterTokui>(reply2.DataMsg) ?? new MasterTokui();
+					sysman.VSoko = new CodeNameView() { Sid = des2.Id, Cd = des2.Code, Mei = des2.Name };
+				}
 			}
 		}
-		var systax = sysman?.Jsub?.Where(x => x.Id == no).FirstOrDefault()??new MasterSysTax();
-		tax = systax.TaxRate;
-		if(Common.CompareYmd(date_ymd, systax.DateFrom) >= 0) {
-			tax = systax.TaxNewRate;
-		}
-		return tax;
+		return sysman;
 	}
 
 
